0a9194a73c5be6d546942688df2bc5f5abd2923d
Jesse Zoldak <jesse.zoldak@clearballot.com>
2022-06-14 22:48:46 -0400

    Fix Lab 4

--- lab_4.py
+++ lab_4.py
@@ -1,15 +1,25 @@
 import boto3
 
 
-def syncTables(event, context):
+def syncTables():
     source_ddb = boto3.client("dynamodb", "us-east-1")
     destination_ddb = boto3.client("dynamodb", "us-west-2")
     sync_ddb_table(source_ddb, destination_ddb)
 
 
-# Scan returns paginated results, so only partial data will be copied
 def sync_ddb_table(source_ddb, destination_ddb):
-    response = source_ddb.scan(TableName="<FMI1>")
+    table = source_ddb.Table("<FMI1")
+    scan_kwargs = {}
+    done = False
+    start_key = None
+    while not done:
+        if start_key:
+            scan_kwargs["ExclusiveStartKey"] = start_key
+        response = table.scan(**scan_kwargs)
+        for item in response["Items"]:
+            destination_ddb.put_item(TableName="<FMI2>", Item=item)
+        start_key = response.get("LastEvaluatedKey", None)
+        done = start_key is None
 
-    for item in response["Items"]:
-        destination_ddb.put_item(TableName="<FMI2>", Item=item)
+
+syncTables()
